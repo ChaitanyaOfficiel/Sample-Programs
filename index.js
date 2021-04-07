a982332ef43c34f121e190518665e6a4a3e0e04d
Chaitanya <vankar_chaitanya@yahoo.com>
2021-04-07 21:09:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var thirdScore = scores[2];
 console.log('firstFruit:', firstFruit);
 console.log('thirdScore:', thirdScore);
 
+// using function making an array of number and desired position or value of array
 function returnAnElement(array, index) {
   return array[index];
 }
@@ -17,6 +18,8 @@ var scores = [98, 85, 91, 78, 82];
 var position = 3;
 var positionElement = returnAnElement(scores, position);
 console.log('positionElement:', positionElement);
+
+// using function making an array of word and getting desired position or value of array
 
 function returnArray(array,index){
   return array[index];
